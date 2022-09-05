4e8896686c41e75d44b66ff4e6edc86153ea24fa
DanilStan <stanislavov.danik@gmail.com>
2022-09-05 18:53:59 +0300

    Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const emptyList = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  // console.log(item);
+  item.textContent = ingredient;
+  item.classList.add('item');
+
+  return item;
+})
+
+const primaryList = document.querySelector('#ingredients');
+primaryList.append(...emptyList);
