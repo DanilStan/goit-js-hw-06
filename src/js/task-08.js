0675fe2fddeb8ad80aa98b4df77d929931614d03
DanilStan <stanislavov.danik@gmail.com>
2022-09-06 12:26:49 +0300

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+const gmailEl = document.querySelector('input[type="email"]')
+const passwordEl = document.querySelector('input[type="password"]')
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (gmailEl.value === '' || passwordEl.value === '') {
+        return alert('All fields must be filled');
+    }
+
+    const formElement = event.currentTarget.elements;
+    const gmail = formElement.email.value;
+    const password = formElement.password.value;
+
+    const userInfo = {
+        gmail,
+        password,
+    }
+
+    console.log(userInfo);
+    form.reset();
+}
