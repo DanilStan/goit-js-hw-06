caad865ca26a06c5a3aca2db04200f1186165e2e
DanilStan <stanislavov.danik@gmail.com>
2022-09-06 23:55:46 +0300

    Task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,43 @@
-const onInputId = document.getElementById('validation-input');
-const onInputIdAttrebuteValue = onInputId.getAttribute('data-length');
+const refs = {
+    inputEl: document.querySelector("#validation-input"),
+}
 
-onInputId.addEventListener('blur', onInputBlur);
+const validLength = Number(refs.inputEl.dataset.length);
+refs.inputEl.addEventListener("blur", onInputValid);
 
-function onInputBlur(event) {
-    // console.dir(event.currentTarget);
-    if (event.currentTarget.value.length === onInputIdAttrebuteValue) {
-        updateClassEl('valid', 'invalid');
+function onInputValid(event) {
+    if (event.target.value.length === validLength) {
+        updateClassElem("valid", "invalid");
     } else {
-        updateClassEl('invalid', 'valid')
+        updateClassElem("invalid", "valid");
     }
 }
 
-function updateClassEl(addClass, remClass) {
-    onInputId.classList.add(addClass)
-    onInputId.classList.remove(remClass)
+function updateClassElem(addCl, remCl) {
+    refs.inputEl.classList.remove(remCl);
+    refs.inputEl.classList.add(addCl);
 }
+
+
+
+
+
+
+// const onInputId = document.querySelector('#validation-input');
+// const onInputIdAttrebuteValue = Number(onInputId.dataset.length);
+
+// onInputId.addEventListener('blur', onInputBlur);
+
+// function onInputBlur(event) {
+//     // console.dir(event.currentTarget);
+//     if (event.target.value.length === onInputIdAttrebuteValue) {
+//         updateClassEl('valid', 'invalid');
+//     } else {
+//         updateClassEl('invalid', 'valid');
+//     }
+// }
+
+// function updateClassEl(addClass, remClass) {
+//     onInputId.classList.add(addClass)
+//     onInputId.classList.remove(remClass)
+// }
